c07d54936ed6a5e2be9aa4e217cf41684e45e525
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-05-03 10:18:40 -0400

    Initial commit

---
+++ com/example/BubbleSort.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 2024-05-03
+ */
+
+// BubbleSort class
+public final class BubbleSort {
+
+    /** Private constructor to prevent instantiation. */
+    private BubbleSort() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
